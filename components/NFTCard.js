2b869a0937cb2704559cc370f4db91b7d29a114a
Davide P <davidepassarini96@gmail.com>
2023-12-08 10:54:29 +0100

    Initial commit, started building cards

---
+++ components/NFTCard.js
@@ -0,0 +1,26 @@
+import {View, Image, Text} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {COLORS, SIZES, SHADOWS, assets} from '../constants';
+
+const NFTCard = ({data}) => {
+  const navigation = useNavigation();
+
+  return (
+    <View
+      style={{
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        marginBottom: SIZES.extralarge,
+        margin: SIZES.base,
+        ...SHADOWS.dark,
+      }}
+    >
+      <View style={{width: '100%', height: 250}}>
+        <Image source={data.image} />
+      </View>
+      <Text>NFTCard</Text>
+    </View>
+  );
+};
+
+export default NFTCard;
